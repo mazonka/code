a2b073a6625c105a947c92af72a8152aea82fea1
Your Name <you@example.com>
2020-06-25 08:13:31 +0400

    z

--- cad/dlib/wget_dlib.sh
+++ cad/dlib/wget_dlib.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 
-z=https://github.com/mazonka/artill/raw/master/src/dlib.tar.bz2
+nm=dlib.tar.bz2
+artdir=../../../artill/src/
 
+artlib=$artdir$nm
+
+if test -f $artlib ; then
+cp $artlib ./
+
+else
+
+z=https://github.com/mazonka/artill/raw/master/src/$nm
 wget --no-check-certificate $z
+
+fi
 
  
