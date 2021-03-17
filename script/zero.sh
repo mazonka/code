e9d88efbae8f8bf64094df3fccbb4efc00e58c93
Your Name <you@example.com>
2021-03-17 06:17:56 +0100

    z

--- script/zero.sh
+++ script/zero.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#  this script prints files that start with 0x00 or empty or non-readable
+#  this script prints files that start with 0's or empty or non-readable
 
 ##IFS='\n'
 
@@ -12,14 +12,14 @@ if test -d "$i"; then
   fun "$1/$i"
   popd > /dev/null
 else
-  zero=`hexdump -e '1/1 "%02.2x"' -n 1 "$i"`
+  zero=`hexdump -e '4/1 "%02.2x"' -n 4 "$i"`
 
   if [ "$zero" = "00" ] ; then
-    echo "[00] $1/$i"
+    echo "[0] $1/$i"
   fi
 
   if [ "$zero" = "" ] ; then
-    echo "[  ] $1/$i"
+    echo "[ ] $1/$i"
   fi
 
   #echo "$1/$i" "[$zero]"
