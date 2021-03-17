a0c5eb488059e2d20751fdc958f92270551bfb1a
Your Name <you@example.com>
2021-03-17 07:14:28 +0100

    z

--- script/zero.sh
+++ script/zero.sh
@@ -6,14 +6,21 @@
 
 shopt -s nullglob
 
+prn(){
+echo -n -e "${old//?/.}\\r"
+echo -n -e "$1\\r"
+old=$1
+}
+
+
 fun(){
 for i in *
 do
 if test -d "$i"; then
+  prn "[$1/$i]"
   pushd "$i" > /dev/null
   fun "$1/$i"
   popd > /dev/null
-  echo -n -e "[$1]  \\r"
 else
   zero=`hexdump -e '4/1 "%02.2x"' -n 4 "$i"`
 
