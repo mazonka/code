d74e7846eaad7ebfaae2e24d4e33595d42c0ccc9
Your Name <you@example.com>
2022-10-09 17:18:47 +0400

    .

--- hideg/bzc.cpp
+++ hideg/bzc.cpp
@@ -98,6 +98,83 @@ void make_key()
 
 }
 
+bool endsWith(string s, string fx)
+{
+    auto sz = s.size();
+    auto fz = fx.size();
+    return sz >= fz && 0 == s.compare(sz - fz, fz, fx);
+};
+
+template <class T>
+inline size_t x2st(T x) { return static_cast<size_t>( x ); }
+string file2str(const string & file)
+{
+    const size_t MAX_FILE_SIZE = 1024u * 1024 * 1000; // 1000Mb
+    std::ifstream in(file.c_str(), std::ios::binary);
+
+    if ( !in )
+        return "";
+
+    string r;
+
+    in.seekg(0, std::ios::end);
+
+    size_t sz = x2st(in.tellg());
+
+    if ( sz > MAX_FILE_SIZE ) throw "File tto big";
+
+    r.reserve( sz );
+    in.seekg(0, std::ios::beg);
+
+    r.assign( std::istreambuf_iterator<char>(in), std::istreambuf_iterator<char>() );
+
+    return r;
+}
+
+
+void run(string file, string hkey)
+{
+    if ( file.empty() ) return;
+
+    cout << "file " << file << '\n';
+
+    string abkey = hash::toBin(hkey);
+    if ( abkey.size() != 32 ) throw "Bad key size";
+    //cout << "bkey.size " << abkey.size() << '\n';
+    //cout << "hkey " << hash::toHex(bkey) << '\n';
+
+    string ofile;
+
+    if (0) {}
+    else    if ( endsWith(file, ".bz2") )
+        ofile = file.substr(0, file.size() - 4) + ".bzc";
+    else    if ( endsWith(file, ".bzc") )
+        ofile = file.substr(0, file.size() - 4) + ".bz2";
+    else throw "File name is not .bz2 or .bzc";
+
+    cout << "Output file: " << ofile << '\n';
+
+    string sfile = file2str(file);
+
+    cout << "File loaded: " << sfile.size() << '\n';
+
+    auto sz = sfile.size();
+    string bkey;
+    for ( size_t i = 0; i < sz; i++ )
+    {
+        if ( !(i % 32) )
+        {
+            bkey = hash::toBin(hkey);
+            hkey = hash::hashHex(hkey);
+        }
+
+        sfile[i] = sfile[i] ^ bkey[i % 32];
+    }
+
+    std::ofstream of(ofile, std::ios::binary);
+    of << sfile;
+}
+
 int main(int ac, const char * av[])
 try
 {
@@ -105,6 +182,7 @@ try
     auto cftime = 1ull * ftime.time_since_epoch().count();
     g_lftime = cftime;
     //cout <<  g_lftime << '\n';
+    g_lftime = 1; // FIXME
     string hash_lftime = hash::hashHex(std::to_string(g_lftime));
 
     g_depth = find_depth();
@@ -126,10 +204,15 @@ try
             cout << "Key [" << keyf.string() << "] expired. Please regenerate\n";
             return 1;
         }
-	key = hash::hashHex(pwd+hash_lftime);
+        key = hash::hashHex(pwd + hash_lftime);
     }
 
-	cout<<"key encr:" << key<<'\n';
+    cout << "key encr: " << key << '\n';
+
+    string file;
+    if ( ac > 1 ) file = av[1];
+
+    run(file, key);
 }
 
 catch (int e)
