8ac2025340e31446cd0fac48d35fec269440709f
Your Name <you@example.com>
2022-10-17 17:14:49 +0400

    .

--- crun/crun.h
+++ crun/crun.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <filesystem>
 #include <fstream>
 #include <iostream>
@@ -47,4 +49,24 @@ catch (...)
     cout << "Unknown exception\n";
     return 1;
 }
+
+
+template<class T> struct ivec : std::vector<T>
+{
+    using Base = std::vector<T>;
+    int size() const { return (int)Base::size(); }
+    void operator+=(const T & b) { Base::push_back(b); }
+    void erase(int i) { Base::erase(Base::begin() + i); }
+};
+
+#ifndef for_i
+#define for_i(x) for(int i=0; i<(x); ++i)
+#define for_ir(x) for(int i=(x); i>=0; --i)
+#define for_(i,x) for(int i=0; i<(x); ++i)
+#endif
+
+#ifndef never
+#define nevers(x) (throw std::string("err at ")+(__FILE__)+":"+std::to_string(__LINE__) + " ["+x+"]")
+#define never (throw std::string("err at ")+(__FILE__)+":"+std::to_string(__LINE__) + " ["+(__func__)+"]")
+#endif
 
