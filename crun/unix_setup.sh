38aba53e5697d73a34d24bf4fa47ede328a01be2
Your Name <you@example.com>
2023-02-07 15:41:48 +0400

    .

--- crun/unix_setup.sh
+++ crun/unix_setup.sh
@@ -4,8 +4,11 @@ echo HOME=$HOME
 
 mkdir -p $HOME/bin
 cp ccrun.exe $HOME/bin/ccrun
+echo "ccrun.exe copied to $HOME/bin"
+
 mkdir -p $HOME/ccrun
-cp compile.ccrun $HOME/bin/ccrun/
+cp compile.ccrun $HOME/ccrun/
+echo "compile.ccrun copied to $HOME/ccrun"
 
 echo "Please edit [$HOME/bin/ccrun/compile.ccrun] to set up the default compilation"
 
