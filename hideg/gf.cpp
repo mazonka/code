51d1706d9b9f8e1464b0d687f19c0e516fa5f625
Your Name <you@example.com>
2023-02-07 14:44:41 +0400

    .

--- hideg/gf.cpp
+++ hideg/gf.cpp
@@ -15,7 +15,7 @@ namespace fs = std::filesystem;
 
 using vs = ivec<string>;
 
-string g_ver = "gf, v1.4.4b, Oleg Mazonka 2022-2023";
+string g_ver = "gf, v1.4.5, Oleg Mazonka 2022-2023";
 
 inline ol::ull gftime()
 {
