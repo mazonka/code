4a9e4b2cc4cbd5581688a00fd1650658b1d8e073
Your Name <you@example.com>
2023-02-13 12:22:07 +0400

    .

---
+++ 1file/cpp2/samedirs.cpp
@@ -0,0 +1,63 @@
+#include "ccrun.h"
+#include "ccrut.h"
+
+double g_thr = 100; // threshold for comparison
+
+//using namespace fsut;
+
+void process_name(string name)
+{
+
+    static string sp = " ";
+
+    //cout << "processing file " << file << '\n';
+
+    if ( g_rep.empty() )
+    {
+        auto i = name.find(sp);
+        if ( i != string::npos )
+        {
+            cout << "[" << name << "]\n";
+            g_are = true;
+        }
+        return;
+    }
+
+    string newn = name;
+    bool withsp = ol::replaceAll(newn, sp, g_rep);
+
+    if ( !withsp ) return;
+
+    fs::rename(name, newn);
+
+    cout << "[" << name << "] -> [" << newn << "]\n";
+}
+
+void process_dir(string dir)
+{
+    {
+        //cout << "processing dir " << dir << '\n';
+        fsut::Pushd pushd(dir);
+        auto ents = fsut::readdir();
+
+        for ( auto f : ents.files().names() ) process_name(f);
+        for ( auto d : ents.dirs().names() ) process_dir(d);
+    }
+
+    // now maybe this dir is also need
+    process_name(dir);
+}
+
+void cmain()
+{
+    ///cout<<args.size();
+    if ( args.size() > 0 ) g_thr = std::stod(args[0]);
+
+    process_dir(".");
+
+    if ( g_are && g_rep.empty() )
+    {
+        cout << "\nspaces found; re-run with arg to replace\n";
+        cout << "e.g. findspace _\n";
+    }
+}
