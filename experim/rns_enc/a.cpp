90bdaee5fb1944f9958eab6ebed42ba67b6982ee
Your Name <you@example.com>
2023-03-02 19:39:38 +0400

    .

---
+++ experim/rns_enc/a.cpp
@@ -0,0 +1,18 @@
+#include <algorithm>
+
+#include "ccrun.h"
+#include "ccrut.h"
+
+void cmain()
+{
+    ol::ivec<int> v;
+    for_( i, 3 ) for_(j, 5) for_(k, 7)
+    {
+        v.push_back( (i * 70 + j * 21 + k * 15) % 105 );
+    }
+
+    std::sort(v.begin(), v.end());
+
+    for ( auto x : v ) cout << ' ' << x;
+	cout<<"\n=["<<v.size()<<"]\n";
+}
