2daf52e7a3aadeaaa7242fa4916f3de5c2510b4c
Your Name <you@example.com>
2023-03-03 21:44:14 +0400

    .

--- experim/gl_equation/oleg_equation.cpp
+++ experim/gl_equation/oleg_equation.cpp
@@ -24,10 +24,11 @@ void cmain()
             bint a = sq(a2);
             if ( a * a == a2 )
             {
-                if ( k <= n )
+                if ( k <= n*n )
                 {
+			cout<<std::dec;
                     cout << "a=" << a << " n=" << n << " k=" << k << " [k<=n]" << '\n';
-                    cout << "a2=" << a2 << std::hex << " a2h=" << a2 << " a=" << a << '\n';
+                    cout << "a2=" << a2 << std::hex << " a2=0x" << a2 << " ah=0x" << a << '\n';
                 }
                 //cout << "a=" << a << " n=" << n << " k=" << k << " sq=" << tsq << '\n';
             }
