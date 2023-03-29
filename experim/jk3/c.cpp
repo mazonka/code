f716c9abc8be5a6488e9542a7f93f8f3efc8252b
Your Name <you@example.com>
2023-03-30 00:26:10 +0400

    .

--- experim/jk3/c.cpp
+++ experim/jk3/c.cpp
@@ -4,7 +4,12 @@
 #include "ccrun.h"
 #include "ccrut.h"
 
-string o(int i)
+using unt = uint64_t;
+
+string unit = "x";
+string zero = " ";
+
+string o(unt i)
 {
     std::ostringstream os;
     if ( !(i / 1000) ) os << ' ';
@@ -14,39 +19,39 @@ string o(int i)
     return os.str();
 }
 
-string bin(int i)
+string bin(unt i)
 {
     string r;
-    while (i) { r = (i % 2 ? "1" : "0") + r; i /= 2; }
+    while (i) { r = (i % 2 ? unit : zero) + r; i /= 2; }
     return r;
 }
 
-int next(int i)
+unt next(unt i)
 {
     i = 3 * i + 1;
     while ( (i / 2) * 2 == i ) i /= 2;
     return i;
 }
 
-string bin(int i, int sz)
+string bin(unt i, unt sz)
 {
     auto r = bin(i);
-    while ( r.size() < sz ) r = "0" + r;
+    while ( r.size() < sz ) r = zero + r;
     return r;
 }
 
 void cmain()
 {
-    int start = (((1 << 10) - 1) << 10 ) + 1;
+    unt start = (((1ull << 1) - 1) << 50 ) + 1;
 
-    for (int i = start; i < start + 30; i += 2)
+    for (unt i = start; i < start + 140; i += 2)
     {
         cout << o(i) << ":\n";
         auto j = i;
-        int cntr = 0;
+        unt cntr = 0;
         while (j != 1)
         {
-            cout << '\t' << bin(j, 30) << '\n';
+            cout << ' ' << bin(j, 64) << '\n';
             j = next(j);
             cntr++;
         }
