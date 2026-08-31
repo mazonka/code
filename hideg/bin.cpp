441f015a6cdc6135aa7645928a65255f2c63f3e1
Your Name <you@example.com>
2026-08-31 22:22:54 +0930

    .

---
+++ hideg/bin.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <map>
+#include <set>
+#include <memory>
+
+#include "olu.h"
+#include "chron.h"
+#include "gfu.h"
+#include "hash.h"
+#include "jadd.h"
+
+using std::cout;
+
+
+int main_bin(ivec<string> args)
+{
+    throw "he";
+}
